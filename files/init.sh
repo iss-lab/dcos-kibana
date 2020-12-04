08ed59b62197171f97c3f61c6a12dc9ef157ae8a
Drew Kerrigan <andrew.kerrigan@issgovernance.com>
2020-12-04 17:01:08 -0500

    Updating

---
+++ files/init.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+set -exo pipefail
+
+export PATH="${PATH}:/usr/sbin"
+export HOME="${MESOS_SANDBOX}"
+
+################################ nginx proxy ###################################
+
+mkdir "${MESOS_SANDBOX}/nginx"
+
+readonly ENV_VARS="\${MESOS_SANDBOX},\${PORT_KIBANA},\${PORT_PROXY},\${FRAMEWORK_NAME},\${MARATHON_APP_LABEL_DCOS_SERVICE_SCHEME}"
+
+envsubst "${ENV_VARS}" < "${MESOS_SANDBOX}/nginx.conf.tmpl" > "${MESOS_SANDBOX}/nginx/nginx.conf"
+
+nginx -c "${MESOS_SANDBOX}/nginx/nginx.conf"
+
+################################# kibana #######################################
+
+readonly KIBANA_PATH="${MESOS_SANDBOX}/kibana-${ELASTIC_VERSION}-linux-x86_64"
+readonly KIBANA_YML_PATH="${KIBANA_PATH}/config/kibana.yml"
+
+if [ -n  "${CUSTOM_YAML_BLOCK_BASE64}" ]; then
+  CUSTOM_YAML_BLOCK=$(echo "${CUSTOM_YAML_BLOCK_BASE64}" | base64 -d)
+fi
+
+cat <<-EOF > "${KIBANA_YML_PATH}"
+	elasticsearch.hosts: ["${ELASTICSEARCH_URL}"]
+	elasticsearch.username: "${KIBANA_USER}"
+	elasticsearch.password: "${KIBANA_PASSWORD}"
+
+	server.host: 0.0.0.0
+	server.port: "${PORT_KIBANA}"
+	server.basePath: "/service/${FRAMEWORK_NAME}"
+	server.rewriteBasePath: true
+
+	# logging.verbose: true
+
+	xpack.security.encryptionKey: "${MESOS_FRAMEWORK_ID}"
+	xpack.reporting.encryptionKey: "${MESOS_FRAMEWORK_ID}"
+
+	${CUSTOM_YAML_BLOCK}
+EOF
+
+if [ -n "$KIBANA_PLUGINS" ]; then
+  echo 'Installing Kibana plugins.'
+  IFS=',' read -ra PLUGINS <<< "$KIBANA_PLUGINS"
+  for plugin in "${PLUGINS[@]}"; do
+    echo "Installing plugin: ${plugin}"
+    ${KIBANA_PATH}/bin/kibana-plugin install "$plugin"
+  done;
+fi
+
+if [ "${KIBANA_ELASTICSEARCH_TLS}" = "true" ]; then
+  cat <<-EOF >> "${KIBANA_YML_PATH}"
+		elasticsearch.ssl.certificateAuthorities: ${MESOS_SANDBOX}/.ssl/ca-bundle.crt
+	EOF
+fi
+
+exec "${MESOS_SANDBOX}/kibana-$ELASTIC_VERSION-linux-x86_64/bin/kibana"
